fdd49e7cd5135599d45cf4b43544afaf328fd90d
ruintel <seeker@etacuder.com>
2023-03-23 17:05:45 +0530

    pic

--- public/static/js/contact.js
+++ public/static/js/contact.js
@@ -1,3 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBhPC9Z4vKAtEwHWixuBt-oBg6z0b1DM50",
+  authDomain: "mangalview-75142.firebaseapp.com",
+  databaseURL: "https://mangalview-75142-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mangalview-75142",
+  storageBucket: "mangalview-75142.appspot.com",
+  messagingSenderId: "558135774564",
+  appId: "1:558135774564:web:e27b9f88d28d640a64fe71",
+  measurementId: "G-6NY3RFPEG9",
+};
 // Get a reference to the Firebase Realtime Database
 var database = firebase.database();
 
